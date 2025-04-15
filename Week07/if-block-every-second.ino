c944a4b2a952e2f612ac761b161b2a82710f3aed
jieun <128567594+zzingni@users.noreply.github.com>
2025-04-15 22:39:22 +0900

    Update code to use SimpleTimer for periodic execution

--- Week07/if-block-every-second.ino
+++ Week07/if-block-every-second.ino
@@ -1,18 +1,22 @@
+#include <SimpleTimer.h>
+
+SimpleTimer timerCnt; // SimpleTimer 클래스로 timerCnt 인스턴스 생성 
+unsigned long loopCnt;
+
+void timerCntFunc() {
+  Serial.println(loopCnt);
+  loopCnt = 0;
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println();
 
+  timerCnt.setInterval(1000, timerCntFunc); // milli-sec, func >> 주기마다 timerCuntFunc 함수 호출 (헤더파일에 그렇게 지정되어 있음)
+
 }
 
 void loop() {
-  static unsigned long loopCnt = 0;
-  static unsigned long nextMil = millis() + 1000;
-
-  loopCnt++;
-
-  if (millis() > nextMil) {
-    nextMil = millis() + 1000;
-    Serial.println(loopCnt);
-    loopCnt = 0;
-  }
+  timerCnt.run();
+  loopCnt++; 
 }
